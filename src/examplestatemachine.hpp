029035fb4e477d2d827bb9d4c962ea29dce43bae
Grubre <ogrodowczykjakub@gmail.com>
2024-05-29 20:29:25 +0200

    Split the state update to two phases 1) changing internal state data 2) maybe going to a new state

--- src/examplestatemachine.hpp
+++ src/examplestatemachine.hpp
@@ -163,8 +163,13 @@ struct StateMachine {
     }
 };
 
-template<typename ...States>
-struct StatelessMachine {
+template <typename T>
+concept State = requires(T t, entt::registry &registry, entt::entity entity) {
+    { t.advance(registry, entity) };
+    { t.change_state(registry, entity) };
+};
+
+template <typename... States> struct StatelessMachine {
     static void iterate_states(auto &&f) { (f.template operator()<States>(), ...); }
 
     template<typename To>
@@ -174,6 +179,24 @@ struct StatelessMachine {
         });
         registry.emplace<To>(entity);
     }
+
+    static void advance_states(entt::registry &registry, entt::entity entity) {
+		iterate_states([&]<typename T>() {
+			const auto view = registry.view<T>();
+            for (auto &&[entity, state] : view.each()) {
+				state.advance(registry, entity);
+			}
+		});
+	}
+    
+    static void change_states(entt::registry &registry, entt::entity entity) {
+		iterate_states([&]<typename T>() {
+			const auto view = registry.view<T>();
+			for (auto &&[entity, state] : view.each()) {
+				state.change_state(registry, entity);
+			}
+		});
+	}
 };
 
 struct EnemyShootingState;
@@ -182,24 +205,30 @@ struct EnemyRunningState;
 using EnemyStatelessMachine = StatelessMachine<EnemyShootingState, EnemyRunningState>;
 
 struct EnemyShootingState {
-    void advance(entt::registry &registry, entt::entity &entity) {
+    void advance(entt::registry &registry, entt::entity entity) {
+        
+    }
+
+    void change_state(entt::registry &registry, entt::entity entity) {
         EnemyStatelessMachine::move_to_state<EnemyRunningState>(registry, entity);
     }
 };
 
 template<typename StMach>
 struct ShotingState {
-    void advance(entt::registry &registry, entt::entity &entity) {
+    void advance(entt::registry &registry, entt::entity entity) {
         StMach::template move_to_state<EnemyRunningState>(registry, entity);
     }
 };
 
 
 struct EnemyRunningState {
-    void advance(entt::registry &registry, entt::entity &entity) {
-        EnemyStatelessMachine::move_to_state<EnemyShootingState>(registry, entity);
+    void advance(entt::registry &registry, entt::entity entity) {
     }
-};
+
+    void change_state(entt::registry &registry, entt::entity entity){
+    	EnemyStatelessMachine::move_to_state<EnemyShootingState>(registry, entity);
+    };
 
 
 
