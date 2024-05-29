d1cff0b845e4ad7d991f3e83ce08976f664fae59
Grubre <ogrodowczykjakub@gmail.com>
2024-05-29 23:20:41 +0200

    Fixed handling of empty types in stateless machine

--- src/examplestatemachine.hpp
+++ src/examplestatemachine.hpp
@@ -7,40 +7,41 @@
 
 namespace bh {
 
-template <typename T>
-concept State = requires(T t, entt::registry &registry, entt::entity entity) {
-    { t.advance(registry, entity) };
-    { t.change_state(registry, entity) };
-};
-
 template <typename... States> struct StatelessMachine {
     static void iterate_states(auto &&f) { (f.template operator()<States>(), ...); }
 
-    template<typename To>
-    static void move_to_state(entt::registry& registry, entt::entity entity) {
-        iterate_states([&]<typename T>() {
-            registry.remove<T>(entity);
-        });
+    template <typename To> static void move_to_state(entt::registry &registry, entt::entity entity) {
+        iterate_states([&]<typename T>() { registry.remove<T>(entity); });
         registry.emplace<To>(entity);
     }
 
     static void advance_states(entt::registry &registry) {
-		iterate_states([&]<typename T>() {
-			const auto view = registry.view<T>();
-            for (auto &&[entity, state] : view.each()) {
-				state.advance(registry, entity);
-			}
-		});
-	}
-    
+        iterate_states([&]<typename T>() {
+            const auto view = registry.view<T>();
+            for (auto entity : view) {
+                if constexpr (std::is_empty_v<T>) {
+                    T::advance(registry, entity);
+                } else {
+                    const auto &state = registry.get<T>(entity);
+                    state.advance(registry, entity);
+                }
+            }
+        });
+    }
+
     static void change_states(entt::registry &registry) {
-		iterate_states([&]<typename T>() {
-			const auto view = registry.view<T>();
-			for (auto &&[entity, state] : view.each()) {
-				state.change_state(registry, entity);
-			}
-		});
-	}
+        iterate_states([&]<typename T>() {
+            const auto view = registry.view<T>();
+            for (auto entity : view) {
+                if constexpr (std::is_empty_v<T>) {
+                    T::change_state(registry, entity);
+                } else {
+                    const auto &state = registry.get<T>(entity);
+                    state.change_state(registry, entity);
+                }
+            }
+        });
+    }
 };
 
 struct EnemyShootingState;
@@ -49,27 +50,23 @@ struct EnemyRunningState;
 using EnemyStatelessMachine = StatelessMachine<EnemyShootingState, EnemyRunningState>;
 
 struct EnemyShootingState {
-    void advance(entt::registry &registry, entt::entity entity) {
-        
-    }
+    static void advance(entt::registry &registry, entt::entity entity) {}
 
-    void change_state(entt::registry &registry, entt::entity entity) {
+    static void change_state(entt::registry &registry, entt::entity entity) {
         EnemyStatelessMachine::move_to_state<EnemyRunningState>(registry, entity);
     }
 };
 
-template<typename StMach>
-struct ShotingState {
-    void advance(entt::registry &registry, entt::entity entity) {
+template <typename StMach> struct ShotingState {
+    static void advance(entt::registry &registry, entt::entity entity) {
         StMach::template move_to_state<EnemyRunningState>(registry, entity);
     }
 };
 
-
 struct EnemyRunningState {
-    void advance(entt::registry &registry, entt::entity entity) {}
+    static void advance(entt::registry &registry, entt::entity entity) {}
 
-    void change_state(entt::registry &registry, entt::entity entity) {
+    static void change_state(entt::registry &registry, entt::entity entity) {
         EnemyStatelessMachine::move_to_state<EnemyShootingState>(registry, entity);
     }
 };
